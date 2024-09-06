2f16061a0ed7f31f41b29e4213d7ff6289ac4a05
hp835 <hp835@host.docker.internal>
2024-09-06 17:39:52 -0600

    Calculadora funcionando con suma
    Soporta suma de enteros exclusivamente

---
+++ src/main/java/mx/com/qtx/calc/test/CalculadoraTest.java
@@ -0,0 +1,14 @@
+package mx.com.qtx.calc.test;
+
+import mx.com.qtx.calc.servicios.Calculadora;
+
+public class CalculadoraTest {
+
+	public static void main(String[] args) {
+		Calculadora miMaizoro = new Calculadora();
+		int resultado = miMaizoro.sumar(100, 150);
+		
+		System.out.println("Resultado suma = " + resultado);
+	}
+
+}
